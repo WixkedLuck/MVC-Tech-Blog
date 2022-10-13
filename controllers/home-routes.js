e095253759840372cb9a29390279c484323b5af7
David Hybner <dah1998@yahoo.com>
2022-10-13 15:34:25 -0400

    update files
    added gitignore file. worked on models for index and comment.js . Added controllers for routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,106 @@
+const { Post, User, Comment } = require('../models');
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  console.log(req.session);
+  Post.findAll({
+    attributes: [
+      'id',
+      'blog_body',
+      'title',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('homepage', { 
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'blog_body',
+      'title',
+      'created_at'
+    ],
+    include: [
+      {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+        
+        // pass data to template
+        res.render('single-post', { 
+          post,
+          loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+    });
+    
+  router.get('/signup', (req, res) => {
+    res.render('signup');
+  });
+
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    
+    res.render('login');
+  });
+
+  router.get('/add-post', (req, res) => {
+    res.render('add-post');
+  })
+
+module.exports = router;
